494d65eefd6d8eb72eb1f3d06f87358f884d1d42
jovan <jovanus@yahoo.co.id>
2025-10-30 00:58:13 +0700

    fix: sitemap

--- src/app/(frontend)/lib/site-config.ts
+++ src/app/(frontend)/lib/site-config.ts
@@ -1,5 +1,5 @@
 export const siteConfig = {
-  domain: "https://analisarchitects.com",
+  domain: "https://www.analisarchitects.com",
   address:
     "Ruko Elang Laut Boulevard Blok D No.60, Pantai Indah Kapuk, Jakarta Utara, Indonesia",
   phone_number: "6287779119390",
